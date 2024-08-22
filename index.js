d09596421ce2174402f2a5a379c1c6249b05617a
Pandorol <820003986@qq.com>
2024-08-22 15:01:48 +0800

    增加适配器
    Signed-off-by: Pandorol <820003986@qq.com>

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 const { instrument } = require("@socket.io/admin-ui");
+const { createAdapter } = require("@socket.io/redis-adapter");
+const { createClient } = require("redis");
 
-instrument(io, {
-    auth: false
-});
+const pubClient = createClient({ url: "redis://8.138.172.203:6379" });
+const subClient = pubClient.duplicate();
+
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
 });
+
+(async () => {
+    try {
+        await Promise.all([pubClient.connect(), subClient.connect()]);
+        io.adapter(createAdapter(pubClient, subClient));
+        instrument(io, { auth: false });
+        console.log(`使用 Redis 适配器的 Socket.IO 服务器已在端口 ${port} 上初始化`);
+    } catch (err) {
+        console.error("Redis 连接失败：", err);
+    }
+})();
+
+
 
 
 // Chatroom
@@ -27,7 +42,7 @@ io.on('connection', (socket) => {
         numUsers: numUsers
     });
     socket.on('message', (data) => {
-        socket.emit('message', {
+        io.emit('message', {
             cmd: 2,
             username: data.username,
             message: data.message
@@ -40,5 +55,8 @@ io.on('connection', (socket) => {
             cmd: 3,
             numUsers: numUsers
         });
+    });
+    socket.on("global", (data) => {
+        socket.emit('message', data);
     });
 });
